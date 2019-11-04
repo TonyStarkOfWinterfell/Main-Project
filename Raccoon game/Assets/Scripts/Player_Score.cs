8f15f11c04fe8e658dbd439c32b540e3b45ac821
stir101 <476395@chrome.escg.ac.uk>
2019-11-04 16:30:47 +0000

    started on score

--- Raccoon game/Assets/Scripts/Player_Score.cs
+++ Raccoon game/Assets/Scripts/Player_Score.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player_Score : MonoBehaviour
 {
 
     public float timeLeft = 120;
-    public int playerscore = 0;
+    public int playerScore = 0;
    
-
-    // Update is called once per frame
     void Update()
     {
+        Debug.Log(playerScore);
         timeLeft -= Time.deltaTime;    
         if (timeLeft <0.1f)
         {
-
+            SceneManager.LoadScene("SampleScene");
         }
     }
+
+    void OnTriggerEnter2D (Collider2D trig)
+    {
+        CountScore();
+    }
+
+    void CountScore()
+    {
+        playerScore = playerScore + (int)(timeLeft * 10);
+        Debug.Log(playerScore);
+    }
+
 }
