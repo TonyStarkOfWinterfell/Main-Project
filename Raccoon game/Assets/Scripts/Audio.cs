7d5619515a604bc58a7854ff9daf571104022ff7
stir101 <476395@chrome.escg.ac.uk>
2019-12-08 23:15:25 +0000

    Started adding audio

---
+++ Raccoon game/Assets/Scripts/Audio.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioClip AppleClip;
+    public AudioClip JumpClip;
+    public AudioClip DeathClip;
+    public AudioSource MusicSource;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicSource.clip = AppleClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MusicSource.clip = AppleClip;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            MusicSource.clip = JumpClip;
+            MusicSource.Play();            
+        }        
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.tag == "Pickapple")
+        {
+            MusicSource.Play();
+        }
+    }
+}
