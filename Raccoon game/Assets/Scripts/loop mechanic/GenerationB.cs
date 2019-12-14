e337fe4ace885ef2017ae5e491e0d3d1da0731e0
stir101 <476395@chrome.escg.ac.uk>
2019-12-14 22:37:23 +0000

    Finished looped mechanic

---
+++ Raccoon game/Assets/Scripts/loop mechanic/GenerationB.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerationB : MonoBehaviour
+{
+
+
+    public GameObject thePlatformB;
+    public Transform generationPointB;
+    public int distanceBetweenB;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > generationPointB.position.x)
+        {
+            transform.position = new Vector3(transform.position.x - distanceBetweenB, transform.position.y, transform.position.z);
+            Instantiate(thePlatformB, transform.position, transform.rotation);
+        }
+    }
+}
